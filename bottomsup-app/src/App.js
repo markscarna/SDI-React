6525c60284f2b949a3f09dac22d44cde132feb0f
markscarna <markscarna@gmail.com>
2022-08-18 09:37:32 -0500

    added func

--- bottomsup-app/src/App.js
+++ bottomsup-app/src/App.js
@@ -10,22 +10,32 @@ class App extends React.Component{
     this.state = {
       defaultCocktails: [],
       searchedCocktails: [],
-      searchInput: ''
+      searchInput: '',
+      isFavorite : false
 
     }
   } 
-  
+
+
+  // componentDidUpdate(){
+  //   console.log('updated')
+  // }
+
   componentDidMount() {
     fetch("http://localhost:3001/")
           .then(res => res.json())
           .then(data => {
             let tempArray = [];
             for(let i = 0; i < 16; i++){
-            tempArray.push(data.drinks[i])
-            }
-            // this.setState({defaultCocktails: CocktailList(data.drinks) })
-            this.setState({defaultCocktails: CocktailList(tempArray) })
-            
+              tempArray.push((<div>
+                <img src={data.drinks[i].strDrinkThumb} width="200" height="300" alt={data.drinks[i].idDrink}></img>
+                <h2>{data.drinks[i].strDrink}</h2>
+                <button onClick={this.handleAddFavorite}>Add To Favorites!</button>
+                <h3>Favorite: {this.state.isFavorite.toString()}</h3>
+                </div> )) 
+            }          
+            this.setState({defaultCocktails: tempArray })                               
+                      
           }) 
           .catch((err) => {
             this.setState({
@@ -34,6 +44,12 @@ class App extends React.Component{
           })             
         
   }
+
+    handleAddFavorite = () => {
+      this.setState({
+        isFavorite : !this.state.isFavorite
+      })
+    }
 
     handleChange = (e) => {
     this.setState({
@@ -59,14 +75,14 @@ class App extends React.Component{
   render() {
     if(this.state.searchedCocktails.length === 0){
 
-      return (<body>
-                <nav id="search-bar">
-                  <input type="search" name="" value={this.state.searchInput} onChange={this.handleChange}></input>
-                  <input type="button" name="" value="Search" onClick={this.handleSubmit}></input>
-                </nav>
-                <label className='labels'>Famous Drinks</label>
+       return (<body>
+                 <nav id="search-bar">
+                   <input type="search" name="" value={this.state.searchInput} onChange={this.handleChange}></input>
+                   <input type="button" name="" value="Search" onClick={this.handleSubmit}></input>
+                 </nav>
+                 <label className='labels'>Famous Drinks</label>
                 <ul className='default-drinks'>{this.state.defaultCocktails}</ul>
-              </body>
+               </body> 
              )
 
     }
